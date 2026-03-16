e0b453b7b62228fa9acd6f0e0955499abfa00310
liemduy <ngocbaheo@gmail.com>
2026-03-16 18:11:02 +0700

    Add files via upload

---
+++ coach2/liemscore/src/config/runtime-config.js
@@ -0,0 +1,8 @@
+(function () {
+    const defaults = {
+        supabaseUrl: 'https://stictpovccelynahbxqt.supabase.co',
+        supabasePublishableKey: 'sb_publishable_VIp3Rp_GKgLT9B3dp92B3A_ZZzygMxd',
+    };
+
+    window.LIEMSCORE_CONFIG = Object.assign({}, defaults, window.LIEMSCORE_CONFIG || {});
+})();
