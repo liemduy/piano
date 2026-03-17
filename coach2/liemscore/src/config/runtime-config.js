cbfd9a2867e34c0e91eed4a47f6b656f8fec1d02
liemduy <ngocbaheo@gmail.com>
2026-03-17 03:24:25 -0300

    Delete coach2/liemscore directory

--- coach2/liemscore/src/config/runtime-config.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    const defaults = {
-        supabaseUrl: 'https://stictpovccelynahbxqt.supabase.co',
-        supabasePublishableKey: 'sb_publishable_VIp3Rp_GKgLT9B3dp92B3A_ZZzygMxd',
-    };
-
-    window.LIEMSCORE_CONFIG = Object.assign({}, defaults, window.LIEMSCORE_CONFIG || {});
-})();
